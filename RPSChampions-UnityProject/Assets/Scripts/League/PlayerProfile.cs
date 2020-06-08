fa04e9d793d0afba8306d108d44d7945f2f4a0e6
Joe <joecgo@gmail.com>
2020-06-07 18:06:12 -0700

    starting on play profiles

--- RPSChampions-UnityProject/Assets/Scripts/League/PlayerProfile.cs
+++ RPSChampions-UnityProject/Assets/Scripts/League/PlayerProfile.cs
@@ -6,6 +6,20 @@ namespace ThirstyJoe.RPSChampions
 
     public class PlayerProfile : MonoBehaviour
     {
+        [SerializeField] private TextMeshProUGUI titleText;
+        [SerializeField] private TextMeshProUGUI statsText;
+        [SerializeField] private TextMeshProUGUI leagueStatsText;
+
+        private void Start()
+        {
+            RequestPlayerInfoFromServer();
+        }
+
+        private void RequestPlayerInfoFromServer()
+        {
+
+        }
+
         public void OnBackButtonPress()
         {
             SceneManager.UnloadSceneAsync("PlayerProfile");
